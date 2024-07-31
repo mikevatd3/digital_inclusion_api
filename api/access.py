f088acf314688eca5b3dfd03caeac49b3198ab72
mikevatd3 <mikevatd3@github.com>
2024-07-31 09:07:35 -0400

    db access added, last commit before deployment

---
+++ api/access.py
@@ -0,0 +1,90 @@
+from sqlalchemy import MetaData, Table, select
+
+
+"""
+Table: acp_claims_by_zipcode
+
+         Column          |         Type          
+-------------------------+-----------------------
+ id                      | bigint                
+ data_month              | date                  
+ zipcode                 | character varying(20) 
+ claimed_subscribers     | double precision      
+ claimed_devices         | double precision      
+ claimed_service_support | double precision      
+ claimed_device_support  | double precision      
+ total_claimed_support   | double precision      
+ zipcode_id              | integer               
+ city_id                 | integer               
+ county_id               | integer               
+
+"""
+
+
+class InvalidFieldException(Exception):
+    """
+    This exception is raised when the field requested on the geography
+    isn't valid.
+    """
+    def __init__(self, *args: object, bad_fields=set()) -> None:
+        """
+        The exception stores which fields didn't work to pass back to the user.
+        """
+        super().__init__(*args)
+        self.bad_fields=bad_fields
+
+
+class ARPASource:
+    def __init__(self, engine) -> None:
+        self.metadata = MetaData()
+        self.engine = engine
+
+        self.acp_claims_by_zipcode = Table("acp_claims_by_zipcode", self.metadata, autoload_with=engine)
+
+
+class ZipCodes:
+    def __init__(self, source: ARPASource) -> None:
+        self.source = source
+        self.available_fields = {
+            "claimed_subscribers",
+            "claimed_devices",
+            "claimed_service_support",
+            "claimed_device_support",
+            "total_claimed_support",
+        }
+
+
+    def get_detail(self, zipcode, db, fields: list[str] = list()):
+        bad_fields = {field for field in fields if field not in self.available_fields}
+        if bad_fields:
+            raise InvalidFieldException(f"Some fields not available on this geographic level.", bad_fields=bad_fields)
+
+        columns = [
+            self.source.acp_claims_by_zipcode.c[field]
+            for field in fields
+        ]
+
+        stmt = select(*columns).where(
+            self.source.acp_claims_by_zipcode.c.zipcode == zipcode
+        )
+
+        return db.execute(stmt).fetchone() 
+
+
+    def get_list(self, filters, db, fields: list[str] = list()):
+        bad_fields = {field for field in fields if field not in self.available_fields}
+        if bad_fields:
+            raise InvalidFieldException(f"The following fields are not available at this geographic level {bad_fields}")
+
+        columns = [
+            self.source.acp_claims_by_zipcode.c[field]
+            for field in fields
+        ]
+
+        stmt = select(*columns)
+
+        return db.execute(stmt).fetchall() 
+
+
+
+
--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import logging
 from api.app_logger import setup_logging
 import tomli
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
+
+from api.access import InvalidFieldException, ARPASource, ZipCodes
+from api.connection import di_engine
 
 
 with open("keys.toml", "rb") as f:
@@ -17,8 +20,17 @@ app = Flask(__name__)
 logger.info("Logger set up correctly!")
 
 
+# Connections section ---- pooling approach tbd
+
+db_source = ARPASource(di_engine)
+zip_codes = ZipCodes(db_source)
+
+# ---- end
+
+
 @app.route("/api/v1/json/healthcheck")
 def healthcheck():
+    logger.info("Healthcheck visited.")
     return jsonify({
         "message": "Looks good so far ..."
     })
@@ -26,16 +38,67 @@ def healthcheck():
 
 @app.route("/api/v1/json/zips")
 def zip_list():
-    return jsonify({
-        "message": "Looks good so far ..."
-    })
+    logger.info("Zip code list view visited.")
+    fields = request.args.get("fields", "").split(",")
+    filters = [] # TODO: What filters are important?
+    
+    try:
+        with di_engine.connect() as db:
+            return jsonify({
+                "geographies": [
+                    geography._asdict()
+                    for geography in zip_codes.get_list(filters, db, fields)
+                ],
+                "metadata": "/api/v1/json/metadata?fields={}"
+            })
+
+    except InvalidFieldException:
+        abort(
+            404, 
+            "Invalid fields requested at the zip-code level, "
+            f"available fields: {zip_codes.available_fields}"
+        )
 
 
-@app.route("/api/v1/json/zips/<zip_code>")
-def zip_detail(zip_code):
-    return jsonify({
-        "message": f"Looks good so far ... for {zip_code}"
-    })
+@app.route("/api/v1/json/zips/<zipcode>")
+def zip_detail(zipcode):
+    logger.info("Zip code detail view visited.")
+
+    fields = request.args.get("fields", "").split(",")
+    filters = [] # TODO: What filters are important?
+    
+    try:
+        with di_engine.connect() as db:
+            return jsonify({
+                "geography": zip_codes.get_detail(zip, db, fields)._asdict(),
+                "metadata": f"/api/v1/json/metadata?fields={request.args.get('fields', '')}"
+            })
+
+    except (AttributeError, InvalidFieldException) as e:
+        match e:
+            case InvalidFieldException():
+                abort(
+                    404, 
+                    f"Invalid fields requested at the zip-code level: {e.bad_fields}, "
+                    f"Available fields: {zip_codes.available_fields}"
+                )
+            case AttributeError():
+                abort(
+                    404, 
+                    f"Invalid zip code requested, {zipcode}."
+                )
+
+
+
+@app.route("/api/v1/json/metadata")
+def metadata():
+    logger.info("Metadata view visited.")
+
+    fields = request.args.get("fields", "").split(",")
+
+    return {
+        "message": f"Metadata endpoint is to come for fields {fields}."
+    }
 
 
 if __name__ == "__main__":
