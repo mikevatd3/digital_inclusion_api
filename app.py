158887691d059e59cb078dd7877b085f7effc650
mikevatd3 <mikevatd3@github.com>
2024-07-31 07:30:06 -0400

    few test endpoints up

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import logging
 from api.app_logger import setup_logging
 import tomli
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 with open("keys.toml", "rb") as f:
@@ -17,12 +17,28 @@ app = Flask(__name__)
 logger.info("Logger set up correctly!")
 
 
-@app.route("/healthcheck")
+@app.route("/api/v1/json/healthcheck")
 def healthcheck():
     return jsonify({
         "message": "Looks good so far ..."
     })
 
 
+@app.route("/api/v1/json/zips")
+def zip_list():
+    return jsonify({
+        "message": "Looks good so far ..."
+    })
+
+
+@app.route("/api/v1/json/zips/<zip_code>")
+def zip_detail(zip_code):
+    return jsonify({
+        "message": f"Looks good so far ... for {zip_code}"
+    })
+
+
 if __name__ == "__main__":
+    setup_logging()
+
     app.run(debug=True)
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -15,10 +15,26 @@ def base_url():
 
 def test_healthcheck_endpoint(base_url):
     request_url = base_url +"/healthcheck"
-    print(request_url)
     response = requests.get(request_url)
-
 
     obj = response.json()
 
     assert obj["message"] == "Looks good so far ..."
+
+
+def test_geos_within_geo(base_url):
+    request_url = base_url + "/zips"
+    response = requests.get(request_url)
+
+    obj = response.json()
+
+    assert obj["message"] == "Looks good so far ..."
+
+
+def test_zip_base(base_url):
+    request_url = base_url + "/zips/48202"
+    response = requests.get(request_url)
+
+    obj = response.json()
+
+    assert obj["message"] == "Looks good so far ... for 48202"
